85c3931424cc2e029d22de672ed1217616faf713
monta <djmonta@gmail.com>
2016-05-13 08:13:10 +0900

    :new: add: userRelated plugin!

---
+++ userRelated.php
@@ -0,0 +1,178 @@
+<?php
+
+/*
+ * This plugin is used to extend the administrator table to add User Profile Picture and Facebook, Twitter, Google+ Integrations.
+ *
+ * <b>NOTE:</b> you must run setup after enabling or disabling this plugin to cause changes to
+ * be made to the database. (Database changes should not be made on an active site.
+ * You should close the site when you run setup.) If you disable the plugin all data
+ * contained in the fields will be discarded.
+ *
+ * @author Sachiko Miyamoto (djmonta)
+ * @package plugins
+ * @subpackage users
+ */
+// force UTF-8 Ø
+
+$plugin_is_filter = 5 | CLASS_PLUGIN;
+$plugin_description = gettext("Add User Profile Picture and Facebook, Twitter, Google+ Integrations.");
+$plugin_author = "Sachiko Miyamoto (djmonta)";
+
+class userRelated {
+	function __construct() {
+		global $_userRelated;
+		$firstTime = extensionEnabled('userRelated') && is_null(getOption('userRelated_addedFields'));
+		$me = 'userRelated';
+		$newfields = self::fields();
+		$previous = getSerializedArray(getOption($me . '_addedFields'));
+		$current = $fields = array();
+		if (extensionEnabled($me)) { //need to update the database tables.
+			foreach ($newfields as $newfield) {
+				$current[$newfield['table']][$newfield['name']] = true;
+				unset($previous[$newfield['table']][$newfield['name']]);
+				switch (strtolower($newfield['type'])) {
+					default:
+						$dbType = strtoupper($newfield['type']);
+						break;
+					case 'int':
+					case 'varchar':
+						$dbType = strtoupper($newfield['type']) . '(' . min(255, $newfield['size']) . ')';
+						break;
+				}
+				$sql = 'ALTER TABLE ' . prefix($newfield['table']) . ' ADD COLUMN `' . $newfield['name'] . '` ' . $dbType;
+				if (query($sql, false) && in_array($newfield['table'], array('albums', 'images', 'news', 'news_categories', 'pages')))
+					$fields[] = strtolower($newfield['name']);
+			}
+			setOption($me . '_addedFields', serialize($current));
+		} else {
+			purgeOption($me . '_addedFields');
+		}
+		$set_fields = array_flip(explode(',', getOption('search_fields')));
+		foreach ($previous as $table => $orpahed) { //drop fields no longer defined
+			foreach ($orpahed as $field => $v) {
+				unset($set_fields[$field]);
+				$sql = 'ALTER TABLE ' . prefix($table) . ' DROP `' . $field . '`';
+				query($sql, false);
+			}
+		}
+		$set_fields = array_unique(array_merge($fields, array_flip($set_fields)));
+		setOption('search_fields', implode(',', $set_fields));
+
+		if ($firstTime) { //	migrate the custom data user data
+			$result = query('SELECT * FROM ' . prefix('administrators') . ' WHERE `valid`!=0');
+			if ($result) {
+				while ($row = db_fetch_assoc($result)) {
+					$custom = getSerializedArray($row['custom_data']);
+					if (!empty($custom)) {
+						$sql = 'UPDATE ' . prefix('administrators') . ' SET ';
+						foreach ($custom as $field => $val) {
+							$sql.= '`' . $field . '`=' . db_quote($val) . ',';
+						}
+						$sql .= '`custom_data`=NULL WHERE `id`=' . $row['id'];
+						query($sql);
+					}
+				}
+				db_free_result($result);
+			}
+		}
+	}
+	static function fields() {
+		return array(
+						array('table' => 'administrators', 'name' => 'profile_picture_url', 'desc' => gettext('Profile Picture URL'), 'type' => 'tinytext')
+		);
+	}
+
+	static function adminSave($updated, $userobj, $i, $alter) {
+		$fields = self::fields();
+		if ($userobj->getValid()) {
+			foreach ($fields as $field) {
+				if (isset($_FILES[$field['name'] . '_' . $i])) {
+					if ($field['name'] == 'profile_picture_url') {
+						$path = self::saveImage($_FILES[$field['name'] . '_' . $i]);
+						$newdata = str_replace(SERVERPATH, FULLWEBPATH, $path);
+						$olddata = $userobj->get($field['name']);
+						$userobj->set($field['name'], $newdata);
+						if ($olddata != $newdata) {
+							$updated = true;
+						}
+					}
+				}
+				//  elseif (isset($_POST[$field['name'] . '_' . $i])) {
+				// 	if ($field['table'] == 'administrators') {
+				// 		$olddata = $userobj->get($field['name']);
+				// 		$userobj->set($field['name'], $newdata = $_POST[$field['name'] . '_' . $i]);
+				// 		if ($olddata != $newdata) {
+				// 			$updated = true;
+				// 		}
+				// 	}
+				// }
+			}
+		}
+		return $updated;
+	}
+
+	static function adminTabs($tabs) {
+		if (isset($tabs['users']['subtabs'])) {
+			$subtabs = $tabs['users']['subtabs'];
+		} else {
+			$subtabs = array();
+		}
+		$subtabs[gettext('users')] = 'admin-users.php?page=users&tab=users';
+		$subtabs[gettext('profile')] = '../' . USER_PLUGIN_FOLDER . '/userRelated/userRelatedTab.php?page=users&tab=profile';
+		$tabs['users'] = array('text'		 => gettext("users"),
+						'link'		 => WEBPATH . "/" . ZENFOLDER . '/admin-users.php?page=users&tab=users',
+						'subtabs'	 => $subtabs,
+						'default'	 => 'users');
+		return $tabs;
+	}
+	/**
+	 * registers filters for handling display and edit of objects as appropriate
+	 */
+	static function register() {
+		$items = array();
+		$fields = self::fields();
+		foreach ($fields as $field) {
+			$items[$field['table']] = true;
+		}
+
+		if (isset($items['administrators'])) {
+			zp_register_filter("save_admin_custom_data", "userRelated::adminSave");
+		// 	zp_register_filter("edit_admin_custom_data", "userRelated::adminEdit");
+		// 	zp_register_filter("admin_head", "userRelated::adminHead");
+			zp_register_filter("admin_tabs", "userRelated::adminTabs");
+		}
+		if (!getOption("userRelated_addedFields")) {
+			zp_register_filter("admin_note", "userRelated::adminNotice");
+		}
+	}
+
+	static function adminNotice($tab, $subtab) {
+		echo '<p class="notebox">' . sprintf(gettext('You will need to run <a href="%1$s">setup</a> to update the database with the custom fields defined by the <em>%2$s</em> plugin.'), FULLWEBPATH . '/' . ZENFOLDER . '/setup.php', 'userRelated') . '</p>';
+	}
+
+	static function getCustomData($obj) {
+		$result = array();
+		$fields = self::fields();
+		foreach ($fields as $element) {
+			if ($element['table'] == $obj->table) {
+				$result[$element['name']] = $obj->get($element['name']);
+			}
+		}
+		return $result;
+	}
+	static function setCustomData($obj, $values) {
+		foreach ($values as $field => $value) {
+			$obj->set($field, $value);
+		}
+	}
+
+}
+
+if (OFFSET_PATH == 2) { // setup call: add the fields into the database
+	setOptionDefault('zp_plugin_userRelated', $plugin_is_filter);
+	new userRelated;
+} else {
+	userRelated::register();
+}
+
+?>
---
+++ userRelated/userRelatedTab.php
@@ -0,0 +1,182 @@
+<?php
+/*
+ * userRelated plugin--tabs
+ * @author Sachiko Miyamoto (djmonta)
+ * @package plugins
+ * @subpackage users
+ */
+define('OFFSET_PATH', 4);
+require_once(dirname(dirname(dirname(__FILE__))) . '/zp-core/admin-globals.php');
+global $_zp_current_admin_obj;
+
+admin_securityChecks(NULL, currentRelativeURL());
+$admins = $_zp_authority->getAdministrators('all');
+
+$ordered = array();
+foreach ($admins as $key => $admin) {
+	if ($admin['valid']) {
+		$ordered[$key] = $admin['date'];
+	}
+}
+asort($ordered);
+$adminordered = array();
+foreach ($ordered as $key => $user) {
+	$adminordered[] = $admins[$key];
+}
+
+$userobj = $_zp_current_admin_obj;
+
+$msg = NULL;
+if (isset($_GET['action'])) {
+	$action = sanitize($_GET['action']);
+	XSRFdefender($action);
+	if ($action == 'profile') {
+		if ($userobj->getValid()) {
+			if (isset($_FILES['profile_picture_url'])) {
+				$path = saveImage($_FILES['profile_picture_url']);
+				$newdata = str_replace(SERVERPATH, FULLWEBPATH, $path);
+				$olddata = $userobj->get('profile_picture_url');
+				$userobj->set('profile_picture_url', $newdata);
+				if ($olddata != $newdata) {
+					//$msg = 'applied';
+					if($olddata) {
+						unlink(str_replace(FULLWEBPATH, SERVERPATH, $olddata));
+					}
+					$updated = true;
+				}
+				zp_apply_filter('save_admin_custom_data', $updated, $userobj, $i, $updated);
+				$userobj->save();
+			}
+		}
+		header("Location: " . FULLWEBPATH . "/" . USER_PLUGIN_FOLDER . '/userRelated/userRelatedTab.php?page=users&tab=profile&applied=' . $msg);
+		exitZP();
+	}
+}
+
+function saveImage($file) {
+	if (isset($file['error']) || is_int($file['error'])) {
+
+		try {
+
+			// $file['error'] の値を確認
+			switch ($file['error']) {
+				case UPLOAD_ERR_OK: // OK
+					break;
+				case UPLOAD_ERR_NO_FILE:   // ファイル未選択
+					continue 2;
+				case UPLOAD_ERR_INI_SIZE:  // php.ini定義の最大サイズ超過
+				case UPLOAD_ERR_FORM_SIZE: // フォーム定義の最大サイズ超過
+					throw new RuntimeException("ファイルサイズが大きすぎます");
+				default:
+					throw new RuntimeException("その他のエラーが発生しました");
+			}
+
+			// $file['mime']の値はブラウザ側で偽装可能なので
+			// MIMEタイプを自前でチェックする
+			if (!$info = @getimagesize($file['tmp_name'])) {
+				throw new RuntimeException("有効な画像ファイルを指定してください");
+			}
+			if (!in_array($info[2], [IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG], true)) {
+				throw new RuntimeException("未対応の画像形式です");
+			}
+
+			// 画像処理に使う関数名を決定する
+			$create = str_replace('/', 'createfrom', $info['mime']);
+			$output = str_replace('/', '', $info['mime']);
+
+			// 縦横比を維持したまま 120 * 120 以下に収まるサイズを求める
+			if ($info[0] >= $info[1]) {
+				$dst_w = 120;
+				$dst_h = ceil(120 * $info[1] / max($info[0], 1));
+			} else {
+				$dst_w = ceil(120 * $info[0] / max($info[1], 1));
+				$dst_h = 120;
+			}
+
+			// 元画像リソースを生成する
+			if (!$src = @$create($file['tmp_name'])) {
+				throw new RuntimeException("画像リソースの生成に失敗しました");
+			}
+
+			// リサンプリング先画像リソースを生成する
+			$dst = imagecreatetruecolor($dst_w, $dst_h);
+
+			// getimagesize関数で得られた情報も利用してリサンプリングを行う
+			imagecopyresampled($dst, $src, 0, 0, 0, 0, $dst_w, $dst_h, $info[0], $info[1]);
+
+			$filename = sprintf('%s%s', sha1_file($file['tmp_name']), image_type_to_extension($info[2]));
+			$filename = SERVERPATH . '/' . USER_PLUGIN_FOLDER . '/userRelated/profile_images/' . $filename;
+			// ファイルデータからSHA-1ハッシュを取ってファイル名を決定し、保存する
+			if (!$output($dst, $filename)) {
+				throw new RuntimeException("ファイル保存時にエラーが発生しました");
+			}
+				$msgs[] = ['green', "リサイズして保存しました"];
+
+		} catch (RuntimeException $e) {
+			$msgs[] = ['red', $e->getMessage()];
+			debugLog($e->getMessage());
+		}
+
+		// リソースを解放
+		if (isset($msg) && is_resource($img)) {
+			imagedestroy($img);
+		}
+		if (isset($dst) && is_resource($dst)) {
+			imagedestroy($dst);
+		}
+		return $filename;
+	} else {
+		debugLog('ERROR: something!');
+	}
+}
+
+printAdminHeader('users');
+?>
+<?php
+echo '</head>' . "\n";
+?>
+
+<body>
+	<?php printLogoAndLinks(); ?>
+	<div id="main">
+		<?php printTabs(); ?>
+		<div id="content">
+			<?php
+			if (isset($_GET['applied'])) {
+				$msg = sanitize($_GET['applied']);
+				if ($msg) {
+					echo "<div class=\"errorbox space\">";
+					echo "<h2>" . $msg . "</h2>";
+					echo "</div>";
+				} else {
+					echo '<div class="messagebox fade-message">';
+					echo "<h2>" . gettext('Processed') . "</h2>";
+					echo '</div>';
+				}
+			}
+			$subtab = printSubtabs();
+			?>
+			<div id="tab_users" class="tabbox">
+				<form action="?action=profile" class="dirty-check" method="post" autocomplete="off" enctype="multipart/form-data">
+					<?php XSRFToken('profile'); ?>
+					<span class="buttons">
+						<button type="submit"><img src="<?php echo FULLWEBPATH . "/" . ZENFOLDER; ?>/images/pass.png" alt="" /><strong><?php echo gettext("Apply"); ?></strong></button>
+						<button type="reset"><img src="<?php echo FULLWEBPATH . "/" . ZENFOLDER; ?>/images/reset.png" alt="" /><strong><?php echo gettext("Reset"); ?></strong></button>
+					</span>
+					<input type="file" size="40" name="profile_picture_url" />
+					<?php if (!empty($userobj->get('profile_picture_url'))) { ?>
+						<img src="<?php echo html_encode($userobj->get('profile_picture_url')); ?>" style="width: 120px; height: auto;" />
+					<?php } ?>
+					<br class="clearall" />
+					<p class="buttons">
+						<button type="submit"><img src="<?php echo FULLWEBPATH . "/" . ZENFOLDER; ?>/images/pass.png" alt="" /><strong><?php echo gettext("Apply"); ?></strong></button>
+						<button type="reset"><img src="<?php echo FULLWEBPATH . "/" . ZENFOLDER; ?>/images/reset.png" alt="" /><strong><?php echo gettext("Reset"); ?></strong></button>
+					</p>
+				</form>
+				<br class="clearall" />
+			</div>
+
+		</div>
+	</div>
+</body>
+</html>
